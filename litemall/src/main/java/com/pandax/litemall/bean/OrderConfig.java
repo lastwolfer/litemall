c0e46e8ed79c43f5c36408a39658a3ba90fb7ea4
lastwolfer <liuguoyu600@yahoo.com>
2019-11-16 12:15:38 +0800

    ok

---
+++ litemall/src/main/java/com/pandax/litemall/bean/OrderConfig.java
@@ -0,0 +1,12 @@
+package com.pandax.litemall.bean;
+
+import lombok.Data;
+
+@Data
+public class OrderConfig {
+
+    private String litemall_order_comment;
+    private String litemall_order_unpaid;
+    private String litemall_order_unconfirm;
+
+}
---
+++ litemall/src/main/java/com/pandax/litemall/bean/WxConfig.java
@@ -0,0 +1,16 @@
+package com.pandax.litemall.bean;
+
+import lombok.Data;
+
+@Data
+public class WxConfig {
+
+    private String litemall_wx_index_new;
+    private String litemall_wx_catlog_goods;
+    private String litemall_wx_catlog_list;
+    private String litemall_wx_share;
+    private String litemall_wx_index_brand;
+    private String litemall_wx_index_hot;
+    private String litemall_wx_index_topic;
+
+}
