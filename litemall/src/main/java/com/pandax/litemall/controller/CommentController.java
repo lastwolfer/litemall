eb3f54382c377f570695fc77d982192a08594dff
o9biv <ggivan0819@163.com>
2019-11-21 22:48:51 +0800

    iooi 13

--- litemall/src/main/java/com/pandax/litemall/controller/CommentController.java
+++ litemall/src/main/java/com/pandax/litemall/controller/CommentController.java
@@ -3,12 +3,17 @@ package com.pandax.litemall.controller;
 import com.github.pagehelper.PageHelper;
 import com.pandax.litemall.bean.BaseReqVo;
 import com.pandax.litemall.bean.Comment;
+import com.pandax.litemall.bean.User;
 import com.pandax.litemall.bean.WxCommentData;
 import com.pandax.litemall.service.CommentService;
+import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,23 +37,7 @@ public class CommentController {
         baseReqVo.setErrmsg("成功");
         return baseReqVo;
     }
-//{
-//    "errno": 0,
-//    "data": {
-//        "data": [{
-//            "userInfo": {
-//                "nickName": "dr lan",
-//                "avatarUrl": ""
-//            },
-//            "addTime": "2019-11-20 04:22:03",
-//            "picList": ["http://192.168.2.100:8081/wx/storage/fetch/6cyaeo310obzfl96c0ck.png"],
-//            "content": "勉勉强强，下次再来（滑稽）\n"
-//        }],
-//        "count": 1,
-//        "currentPage": 1
-//    },
-//    "errmsg": "成功"
-//}
+
     @RequestMapping("wx/comment/list")
     public BaseReqVo commentList(int valueId,byte type,int size,int page,int showType){
         List<WxCommentData> comments = commentService.getCommentsList(valueId,type,size,page,showType);
@@ -63,5 +52,44 @@ public class CommentController {
 
         return baseReqVo;
     }
-
+//wx/comment/post
+    //content: "123"
+    //hasPicture: false
+    //picUrls: []
+    //star: 4
+    //type: 1
+    //valueId: "358"
+    @RequestMapping("wx/comment/post")
+    public BaseReqVo commentPost(Comment comment) throws ParseException {
+        User user = (User) SecurityUtils.getSubject().getPrincipal();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = df.parse(df.format(new Date()));
+        comment.setUserId(user.getId());
+        comment.setAddTime(date);
+        comment.setUpdateTime(date);
+        int i = commentService.commentPost(comment);
+        BaseReqVo<Object> baseReqVo = new BaseReqVo<>();
+        Map<String, Object> map = new HashMap<>();
+        map.put("data", comment);
+        baseReqVo.setErrno(0);
+        baseReqVo.setData(map);
+        baseReqVo.setErrmsg("成功");
+        return baseReqVo;
+    }
+    //{
+    //    "errno": 0,
+    //    "data": {
+    //        "id": 1114,
+    //        "valueId": 358,
+    //        "type": 1,
+    //        "content": "123",
+    //        "userId": 7,
+    //        "hasPicture": false,
+    //        "picUrls": [],
+    //        "star": 4,
+    //        "addTime": "2019-11-21 09:23:23",
+    //        "updateTime": "2019-11-21 09:23:23"
+    //    },
+    //    "errmsg": "成功"
+    //}
 }
--- litemall/src/main/java/com/pandax/litemall/mapper/KeywordMapper.java
+++ litemall/src/main/java/com/pandax/litemall/mapper/KeywordMapper.java
@@ -31,8 +31,8 @@ public interface KeywordMapper {
 
     List<Keyword> selectByCondition(@Param("keyword") String keyword,@Param("url") String url, @Param("sort") String sort, @Param("order") String order);
 
-    @Select("select * from cskaoyan_mall_keyword where is_default = 1")
-    List<Keyword> selectDefault();
+    @Select("select * from cskaoyan_mall_keyword where is_default = 1 ORDER BY rand() LIMIT 1")
+    Keyword selectDefault();
 
     @Select("select * from cskaoyan_mall_keyword where is_hot = 1 limit 20")
     List<Keyword> selectHot();
